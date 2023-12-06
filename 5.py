351c6cd8bfde8abfa59098350bcbde3a4adeab35
MohamedKiouaz <mohamed.kiouaz@gmail.com>
2023-12-06 08:17:05 +0100

    update

--- 5.py
+++ 5.py
@@ -17,38 +17,51 @@ class RangeTransform:
     r: int
 
     def __str__(self) -> str:
-        return f"{self.source} (+ {self.r}) -> {self.destination}"
+        return f"{self.source}-{self.source+self.r-1} -({self.destination-self.source})-> {self.destination}-{self.destination+self.r-1}"
     
     def __repr__(self) -> str:
         return self.__str__()
 
     def split_by_dest(self, dest_number):
-        if dest_number < self.destination or dest_number > self.destination + self.r:
+        if dest_number <= self.destination or dest_number >= self.destination + self.r:
             return []
         else:
             left_r = dest_number - self.destination
             return [RangeTransform(self.destination, self.source, left_r), RangeTransform(dest_number, self.source + left_r, self.r - left_r)]
     
     def split_by_source(self, source_number):
-        if source_number < self.source or source_number > self.source + self.r:
+        if source_number <= self.source or source_number > self.source + self.r:
             return []
         else:
             left_r = source_number - self.source
             return [RangeTransform(self.destination, self.source, left_r), RangeTransform(self.destination + left_r, self.source + left_r, self.r - left_r)]
+    
+    def merge(self, other):
+        if self.source == other.source and self.destination == other.destination:
+            return RangeTransform(other.destination, self.source, max(self.r, other.r))
+        elif self.destination - self.source == other.destination - other.source and self.source + self.r >= other.source:
+            source = min(self.source, other.source)
+            dest = min(self.destination, other.destination)
+            r = max(self.r, other.r)
+            return RangeTransform(dest, source, r)
+        else:
+            return None
         
-    def __add__(self, other, level = 0):
-        if level == 3:
-            return []
+    def is_identity(self):
+        return self.destination == self.source
         
-        print(f'add {level}', self, other)
-
+    def stack(self, other):
         if self.destination == other.source and self.r == other.r:
-            print('same')
             return [RangeTransform(other.destination, self.source, self.r)]
         
-        if self.destination + self.r <= other.source or other.source + other.r <= self.destination:
-            print('no overlap')
+        if other.is_identity():
             return [self]
+
+        if self.destination + self.r <= other.source or other.source + other.r <= self.destination:
+            #print('no overlap')
+            return [self]
+        
+        print(f'stack', self, "/", other)
         
         my_ranges = self.split_by_dest(max(self.destination, other.source))
         my_ranges += self.split_by_dest(min(self.destination + self.r, other.source + self.r))
@@ -56,34 +69,43 @@ class RangeTransform:
         other_ranges = other.split_by_source(max(self.destination, other.source))
         other_ranges += other.split_by_source(min(self.destination + self.r, other.source + other.r))
 
-        L = []
+        dup_ranges = []
 
         for my_range in my_ranges:
             for other_range in other_ranges:
                 if my_range.r != 0 and other_range.r != 0:
-                    #L.extend(my_range + other_range)
-                    L.extend(my_range.__add__(other_range, level + 1))
+                    dup_ranges.extend(my_range.stack(other_range))
 
-        L = [x for x in L if x]
+        dup_ranges = [x for x in dup_ranges if x]
 
-        ret = []
-        for x in L:
-            if x not in ret and not x.r == 0:
-                ret.append(x)
-        return ret
+        unique_ranges = []
+        for x in dup_ranges:
+            if x not in unique_ranges and not x.r == 0:
+                unique_ranges.append(x)
+
+        print(unique_ranges)
+
+        return unique_ranges
     
     def __eq__(self, __value: object) -> bool:
         return self.destination == __value.destination and self.source == __value.source and self.r == __value.r
 
-print(RangeTransform(0, 0, 300))
-print(RangeTransform(10, 10, 10))
-print("Sum", RangeTransform(15, 0, 15) + RangeTransform(10, 5, 35))
+# test split
+# print(RangeTransform(0, 0, 10).split_by_dest(5))
+# print(RangeTransform(0, 0, 10).split_by_dest(15))
+# print(RangeTransform(0, 0, 10).split_by_dest(0))
 
-exit()     
+# print(RangeTransform(0, 0, 10).split_by_source(5))
+# print(RangeTransform(0, 0, 10).split_by_source(15))
+# print(RangeTransform(0, 0, 10).split_by_source(0))
 
-print(RangeTransform(5, 0, 15))
-print(RangeTransform(100, 10, 10))
-print("Sum", RangeTransform(5, 0, 15) + RangeTransform(100, 10, 10))
+# print("Sum", RangeTransform(15, 0, 15) + RangeTransform(10, 5, 35))
+# print(RangeTransform(15, 0, 15))
+# print(RangeTransform(10, 5, 35))
+
+# print("Sum", RangeTransform(5, 0, 15) + RangeTransform(100, 10, 10))
+# print(RangeTransform(5, 0, 15))
+# print(RangeTransform(100, 10, 10))
 
 class Transform:
     def __init__(self, name):
@@ -105,15 +127,24 @@ class Transform:
             else:
                 yield number
 
-    def __add__(self, other):
+    def stack(self, other):
         new_map = Transform(f"{self.name} + {other.name}")
-        new_map.maps = self.maps
-        for other_map in other.maps:
-            for map in new_map.maps:
-                if map.destination == other_map.source:
-                    map.destination = other_map.destination
-                    map.r += other_map.r
-                    break
+
+        stacked_transform_ranges = []
+        for map in self.maps:
+            for other_map in other.maps:
+                stacked_transform_ranges.extend(map.stack(other_map))
+
+        stacked_transform_ranges = sorted(stacked_transform_ranges, key=lambda x: x.source)
+        
+        print(stacked_transform_ranges)
+
+        for range in stacked_transform_ranges:
+            if len(new_map.maps) >= 1 and range.merge(new_map.maps[-1]) is not None:
+                new_map.maps[-1] = range.merge(new_map.maps[-1])
+            else:
+                new_map.maps.append(range)
+
         return new_map
 
 maps = []
@@ -129,6 +160,27 @@ for line in content[1:]:
         r = int(r.strip())
         current_map.maps.append(RangeTransform(dest, source, r))
 
+a = Transform("a")
+a.maps.append(RangeTransform(5, 0, 10))
+a.maps.append(RangeTransform(15, 10, 10))
+b = Transform("b")
+b.maps.append(RangeTransform(1, 0, 20))
+b.maps.append(RangeTransform(19, 20, 10))
+
+print(a)
+for map in a.maps:
+    print(map)
+
+print(b)
+for map in b.maps:
+    print(map)
+
+print(a.stack(b))
+for map in a.stack(b).maps:
+    print(map)
+
+exit()
+
 print(maps)
 
 results = seeds
@@ -137,6 +189,9 @@ for map in maps:
 
 print(results)
 print(min(results))
+
+final_transfo = reduce(lambda x, y: x.stack(y), maps)
+print(final_transfo)
 
 min_ = 1e10
 for i in tqdm(range(0, len(seeds), 2)):
