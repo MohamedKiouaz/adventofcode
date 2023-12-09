a03a940b2ced4d35df6f21c7f41fb42b050d334f
Mohamed Kiouaz <mohamed.kiouaz@gmail.com>
2023-12-09 09:40:31 +0100

    Add day 9

---
+++ 2023/9/9b.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+with open('2023/9/9.txt') as f:
+    content = f.readlines()
+
+content = [x.strip().lower() for x in content]
+
+def work(line):
+    numbers = line.split(' ')
+    numbers = np.array([int(k.strip()) for k in numbers])
+    
+    return predict_next(numbers)
+
+def predict_next(numbers):
+    print(numbers)
+    if len(numbers) < 2:
+        print('bad input')
+        return 0
+    
+    n_ = numbers[1:] - numbers[:-1]
+
+    if np.all(n_ == n_[0]):
+        return numbers[0] - n_[0]
+    else:
+        return numbers[0] - predict_next(n_)
+    
+res = list(map(work, content))
+print(res)
+
+print(sum(res))
